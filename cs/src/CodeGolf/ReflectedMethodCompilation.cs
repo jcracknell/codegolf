17b895d59ab1234387002c1fe67c751d1731f654
James Cracknell <james.h.cracknell@gmail.com>
2013-02-02 11:51:29 -0700

    ReflectedMethodCompilation experiment

---
+++ cs/src/CodeGolf/ReflectedMethodCompilation.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace CodeGolf {
+	/// <summary>
+	/// <para>
+	/// Delegate type representing a reflected function which has been compiled for high performance invocation.
+	/// </para>
+	/// </summary>
+	/// <param name="site">The site (instance) at which the compiled function should be invoked. Unused by static methods.</param>
+	/// <param name="args">The arguments with which the compiled function should be invoked.</param>
+	public delegate object CompiledReflectedFunction(object site, object[] args);
+
+	/// <summary>
+	/// Delegate type representing a reflected action which has been compiled for high performance invocation.
+	/// </summary>
+	/// <param name="site">The site (instance) at which the compiled action should be invoked. Unused by static methods.</param>
+	/// <param name="args">The arguments with which the compiled action should be invoked.</param>
+	public delegate void CompiledReflectedAction(object site, object[] args);
+
+	/// <summary>
+	/// <para>
+	/// Static class providing facilities for the compilation of reflected methods for high performance invocation.
+	/// </para>
+	/// <para>
+	/// Compilation generally takes less than a millisecond, with performance gains generally recouping the cost
+	/// of compilation in less than 1000 invocations.
+	/// </para>
+	/// </summary>
+	public static class ReflectedMethodCompilation {
+
+		/// <summary>
+		/// Compile the provided <paramref name="function"/> to a delegate.
+		/// </summary>
+		/// <param name="function">The function to be compiled to a delegate.</param>
+		public static CompiledReflectedFunction CompileFunction(MethodInfo function) {
+			if(null == function) throw Xception.Because.ArgumentNull(() => function);
+			if(typeof(void) == function.ReturnType) throw Xception.Because.Argument(() => function, "has a void return type");
+
+			var siteParameter = Expression.Parameter(typeof(object), "o");
+			var argsParameter = Expression.Parameter(typeof(object[]), "arg");
+
+			return Expression.Lambda<CompiledReflectedFunction>(
+					Expression.Convert(MakeBodyExpression(function, siteParameter, argsParameter), typeof(object)),
+					new ParameterExpression[] { siteParameter, argsParameter }
+				)
+				.Compile();
+		}
+
+		/// <summary>
+		/// Compile the provided generic <paramref name="function"/> to a delegate using the provided <paramref name="typeParameters"/>.
+		/// </summary>
+		/// <param name="function">The generic function to be compiled to a delegate.</param>
+		/// <param name="typeParameters">The type parameters with which the generic function should be compiled.</param>
+		public static CompiledReflectedFunction CompileFunction(MethodInfo function, IEnumerable<Type> typeParameters) {
+			if(null == function) throw Xception.Because.ArgumentNull(() => function);
+			if(null == typeParameters) throw Xception.Because.ArgumentNull(() => typeParameters);
+			if(typeof(void) == function.ReturnType) throw Xception.Because.Argument(() => function, "has a void return type");
+
+			return CompileFunction(MakeGenericMethod(function, typeParameters));
+		}
+
+		/// <summary>
+		/// Compile the provided <paramref name="action"/> to a delegate.
+		/// </summary>
+		/// <param name="action">The action to be compiled to a delegate.</param>
+		public static CompiledReflectedAction CompileAction(MethodInfo action) {
+			if(null == action) throw Xception.Because.ArgumentNull(() => action);
+			if(typeof(void) != action.ReturnType) throw Xception.Because.Argument(() => action, "does not have a void return type");
+
+			var siteParameter = Expression.Parameter(typeof(object), "o");
+			var argsParameter = Expression.Parameter(typeof(object[]), "arg");
+			var body = MakeBodyExpression(action, siteParameter, argsParameter);
+			var expression = Expression.Lambda<CompiledReflectedAction>(body, new ParameterExpression[] { siteParameter, argsParameter });
+
+			return expression.Compile();
+		}
+
+		/// <summary>
+		/// Compile the provided generic <paramref name="action"/> to a delegate using the provided <paramref name="typeParameters"/>.
+		/// </summary>
+		/// <param name="action">The generic action which should be compiled to a delegate.</param>
+		/// <param name="typeParameters">The type parameters with which the generic action should be compiled.</param>
+		public static CompiledReflectedAction CompileAction(MethodInfo action, IEnumerable<Type> typeParameters) {
+			if(null == action) throw Xception.Because.ArgumentNull(() => action);
+			if(null == typeParameters) throw Xception.Because.ArgumentNull(() => typeParameters);
+			if(typeof(void) != action.ReturnType) throw Xception.Because.Argument(() => action, "does not have a void return type");
+
+			return CompileAction(MakeGenericMethod(action, typeParameters));
+		}
+
+		private static MethodInfo MakeGenericMethod(MethodInfo method, IEnumerable<Type> typeParameters) {
+			if(!method.IsGenericMethodDefinition)
+				method = method.GetGenericMethodDefinition();
+
+			return method.MakeGenericMethod(typeParameters.ToArray());
+		}
+
+		private static Expression MakeBodyExpression(MethodInfo method, ParameterExpression siteParameter, ParameterExpression argsParameter) {
+			var argumentExpressions = new LinkedList<Expression>();
+			foreach(var methodParameter in method.GetParameters()) {
+				Expression argumentExpression = Expression.ArrayIndex(argsParameter, Expression.Constant(methodParameter.Position));
+
+				if(!typeof(object).Equals(methodParameter.ParameterType))
+					argumentExpression = Expression.Convert(argumentExpression, methodParameter.ParameterType);
+
+				argumentExpressions.AddLast(argumentExpression);
+			}
+
+			var siteExpression = method.IsStatic ? null : Expression.Convert(siteParameter, method.DeclaringType);
+
+			return Expression.Call(siteExpression, method, argumentExpressions);
+		}
+	}
+}
---
+++ cs/src/CodeGolf/ReflectedMethodCompilationTests.cs
@@ -0,0 +1,170 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace CodeGolf {
+	public class ReflectedMethodCompilationTests {
+		class Clazz {
+			public string InstanceField = "foo";
+			public static string StaticField;
+
+			static Clazz() { Reset(); }
+			public static void Reset() { StaticField = "foo"; }
+
+			public int BenchmarkFunction() { return 42; }
+
+			public string InstanceFunction(int i, int j) {
+				return string.Concat(this.InstanceField,(i + 2 * j) * 3);
+			}
+
+			public string GenericInstanceFunction<T>() {
+				return InstanceField + typeof(T).Name;
+			}
+
+			public static string StaticFunction(char c, int r) {
+				Reset();
+				return StaticField + new string(c, r);
+			}
+
+			public static IEnumerable<T> GenericStaticFunction<T>(T item) {
+				Reset();
+				return new T[] { item };
+			}
+
+			public void InstanceAction(int i, int j) {
+				InstanceField = "bar" + (i * j);
+			}
+
+			public void GenericInstanceAction<T>() {
+				InstanceField = typeof(T).Name;
+			}
+
+			public static void StaticAction(string s, int i) {
+				Reset();
+				StaticField = s.ToString() + (2 * i).ToString();
+			}
+
+			public static void GenericStaticAction<T>() {
+				Reset();
+				StaticField = typeof(T).Name;
+			}
+		}
+
+		private static readonly MethodInfo InstanceFunction = typeof(Clazz).GetMethod("InstanceFunction");
+		private static readonly MethodInfo GenericInstanceFunction = typeof(Clazz).GetMethod("GenericInstanceFunction");
+		private static readonly MethodInfo StaticFunction = typeof(Clazz).GetMethod("StaticFunction");
+		private static readonly MethodInfo GenericStaticFunction = typeof(Clazz).GetMethod("GenericStaticFunction");
+		private static readonly MethodInfo InstanceAction = typeof(Clazz).GetMethod("InstanceAction");
+		private static readonly MethodInfo GenericInstanceAction = typeof(Clazz).GetMethod("GenericInstanceAction");
+		private static readonly MethodInfo StaticAction = typeof(Clazz).GetMethod("StaticAction");
+		private static readonly MethodInfo GenericStaticAction = typeof(Clazz).GetMethod("GenericStaticAction");
+
+		[Fact] public void ReflectedMethodCompilation_CompileFunction_should_work_for_instance_function() {
+			var compiled = ReflectedMethodCompilation.CompileFunction(InstanceFunction);
+			compiled(new Clazz(), new object[] { 1, 2 }).Should().Be("foo15");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileFunction_should_work_for_generic_function() {
+			var compiled = ReflectedMethodCompilation.CompileFunction(GenericInstanceFunction, new Type[] { typeof(List<DateTime>) });
+			compiled(new Clazz(), new object[0]).Should().Be("fooList`1");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileFunction_should_work_for_static_function() {
+			var compiled = ReflectedMethodCompilation.CompileFunction(StaticFunction);
+			compiled(null, new object[] { 'x', 6 }).Should().Be("fooxxxxxx");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileFunction_should_work_for_generic_static_function() {
+			var compiled = ReflectedMethodCompilation.CompileFunction(GenericStaticFunction, new Type[] { typeof(string) });
+			var result = compiled(null, new object[] { "fizz" }) as IEnumerable<string>;
+			result.Should().BeEquivalentTo(new string[] { "fizz" });
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileAction_should_work_for_instance_action() {
+			var instance = new Clazz();
+			var compiled = ReflectedMethodCompilation.CompileAction(InstanceAction);
+			compiled(instance, new object[] { 2, 7 });
+
+			instance.InstanceField.Should().Be("bar14");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileAction_should_work_for_generic_instance_action() {
+			var instance = new Clazz();
+			var compiled = ReflectedMethodCompilation.CompileAction(GenericInstanceAction, new Type[] { typeof(string) });
+
+			compiled(instance, new object[0]);
+
+			instance.InstanceField.Should().Be("String");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileAction_should_work_for_static_action() {
+			var compiled = ReflectedMethodCompilation.CompileAction(StaticAction);
+			compiled(null, new object[] { "bar", 4 });
+
+			Clazz.StaticField.Should().Be("bar8");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_CompileAction_should_work_for_generic_static_action() {
+			var compiled = ReflectedMethodCompilation.CompileAction(GenericStaticAction, new Type[] { typeof(IEnumerable<string>) });
+			compiled(null, new object[0]);
+
+			Clazz.StaticField.Should().Be("IEnumerable`1");
+		}
+
+		[Fact] public void ReflectedMethodCompilation_benchmark() {
+			for(var i = 10; i <= 1000000; i = i * 10)
+				BenchmarkIterations(i);
+		}
+
+		private static void BenchmarkIterations(int iterations) {
+			var benchmarkFunction = typeof(Clazz).GetMethod("BenchmarkFunction");
+			var site = new Clazz();
+			var args = new object[0];
+
+			int i = 0;
+			var stopwatch = new System.Diagnostics.Stopwatch();
+
+			stopwatch.Reset();
+			stopwatch.Start();
+
+			for(i = 0; i != iterations; i++)
+				benchmarkFunction.Invoke(site, args);
+
+			stopwatch.Stop();
+
+			var uncompiledRuntime = stopwatch.ElapsedTicks;
+
+			stopwatch.Reset();
+			stopwatch.Start();
+
+			var compiled = ReflectedMethodCompilation.CompileFunction(benchmarkFunction);
+
+			stopwatch.Stop();
+
+			var compilationTime = stopwatch.ElapsedTicks;
+
+			stopwatch.Reset();
+			stopwatch.Start();
+
+			for(i = 0; i != iterations; i++)
+				compiled(site, args);
+
+			stopwatch.Stop();
+
+			var compiledRuntime = stopwatch.ElapsedTicks;
+
+			double relative = ((double)compiledRuntime) / ((double)uncompiledRuntime) * 100d;
+			double increase = 100d / relative;
+
+			Console.WriteLine("=== " + iterations + " iterations ===");
+			Console.WriteLine("Compilation: " + compilationTime);
+			Console.WriteLine("Uncompiled: " + uncompiledRuntime);
+			Console.WriteLine("Compiled: " + compiledRuntime);
+			Console.WriteLine("Runtime Change: " + ((double)uncompiledRuntime / (double)(compilationTime + compiledRuntime)) * 100d + "%");
+		}
+	}
+}
