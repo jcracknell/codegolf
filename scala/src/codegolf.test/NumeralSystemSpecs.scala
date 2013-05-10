5a43107075b229b286aff13b3bba3241b238c32f
James Cracknell <james.h.cracknell@gmail.com>
2013-05-09 21:42:24 -0600

    Added NumeralSystem experiments

---
+++ scala/src/codegolf.test/NumeralSystemSpecs.scala
@@ -0,0 +1,67 @@
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class NumeralSystemSpecs extends FunSpec with ShouldMatchers {
+  describe("NumeralSystem") {
+    describe("Alpha") {
+      describe("decode method") {
+        it("should decode a") {
+          NumeralSystem.Alpha.decode("a") should be (Some(1))
+        }
+        it("should decode b") {
+          NumeralSystem.Alpha.decode("b") should be (Some(2))
+        }
+        it("should decode z") {
+          NumeralSystem.Alpha.decode("z") should be (Some(26))
+        }
+        it("should decode aa") {
+          NumeralSystem.Alpha.decode("aa") should be (Some(27))
+        }
+        it("should decode AA") {
+          NumeralSystem.Alpha.decode("AA") should be (Some(27))
+        }
+      }
+      describe("encode method") {
+        it("should return None for 0") {
+          NumeralSystem.Alpha.encode(0) should be (None)
+        }
+        it("should return None for -1") {
+          NumeralSystem.Alpha.encode(-1) should be (None)
+        }
+        it("should encode 1") {
+          NumeralSystem.Alpha.encode(1) should be (Some("a"))
+        }
+        it("should encode 2") {
+          NumeralSystem.Alpha.encode(2) should be (Some("b"))
+        }
+        it("should encode 26") {
+          NumeralSystem.Alpha.encode(26) should be (Some("z"))
+        }
+        it("should encode 27") {
+          NumeralSystem.Alpha.encode(27) should be (Some("aa"))
+        }
+        it("should encode 53") {
+          NumeralSystem.Alpha.encode(53) should be (Some("ba"))
+        }
+        it("should encode 54") {
+          NumeralSystem.Alpha.encode(57) should be (Some("bb"))
+        }
+        it("should encode 153") {
+          NumeralSystem.Alpha.encode(153) should be (Some("ew"))
+        }
+        it("should encode 267") {
+          NumeralSystem.Alpha.encode(267) should be (Some("jg"))
+        }
+        it("should encode 329") {
+          NumeralSystem.Alpha.encode(329) should be (Some("lq"))
+        }
+        it("should encode 17603") {
+          NumeralSystem.Alpha.encode(17603) should be (Some("zaa"))
+        }
+        it("should encode Int.MaxValue") {
+          NumeralSystem.Alpha.encode(Int.MaxValue) should be (Some("fxshrxw"))
+        }
+      }
+    }
+  }
+}
---
+++ scala/src/codegolf/NumeralSystem.scala
@@ -0,0 +1,57 @@
+/** @define NS `NumeralSystem`
+ *  @define ns numeral system
+ *
+ *  @author James Cracknell
+ *
+ *  Trait describing a $ns capable of encoding and decoding integer values to and from
+ *  a corresponding string representation.
+ */
+trait NumeralSystem {
+  /** Attempts to decode the provided `String` value `s` according to the $NS, returning the
+   *  corresponding `Int` value if successful, `None` otherwise.
+   *
+   *  @param s The `String` value to be decoded.
+   *  @return The corresponding `Int` value if successful, `None` otherwise.
+   */
+  def decode(s:String):Option[Int]
+
+  /** Attempts to encode the provided `Int` value `i` according to the $NS, returning a corresponding
+   *  `String` representation if `i` can be encoded, `None` otherwise.
+   *
+   *  @param i The `Int` value to be encoded.
+   *  @return A corresponding `String` representation if `i` can be encoded, `None` otherwise.
+   */
+  def encode(i:Int):Option[String]
+}
+
+object NumeralSystem {
+  /** `NumeralSystem` encoding positive integers using roman letters 'a' through 'z'. */
+  object Alpha extends NumeralSystem {
+    val CharValues = (('a' to 'z') map { c => (c, c-'a'+1) }) ++ (('A' to 'Z') map { c => (c, c-'A'+1) }) toMap
+
+    def decode(s:String):Option[Int] =
+      (Option(0) /: s) { (m,c) =>
+        m match {
+          case Some(x) => CharValues.get(c) map { _ + 26 * x }
+          case None => m
+        }
+      }
+
+    def encode(i:Int):Option[String] =
+      if(1 > i) Option.empty[String]
+      else {
+        val ds = Iterator.iterate(i-1)(_/26 - 1) takeWhile(0<=)
+        Some((ds :\ new StringBuilder) { (x, sb) => sb + ('a'+x%26).toChar } toString)
+      }
+  }
+
+  /** `NumeralSystem` encoding integers as arabic numerals, the numeral system with which
+   *  most people are familiar. */
+  object Arabic extends NumeralSystem {
+    def decode(s:String): Option[Int] =
+      try { Some(Integer.parseInt(s)) }
+      catch { case _:Throwable => Option.empty[Int] }
+
+    def encode(i:Int): Option[String] = Some(i.toString)
+  }
+}
